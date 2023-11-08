fcb480d02c10e4afaa62afb7aaf086079b237b5a
Sathishkumar <sathishkumar@Sathishkumars-MacBook-Air.local>
2023-11-08 15:25:50 +0530

    db connection

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 // files
 const dbConnect = require('./db/dbconnection');
 const book =  require ('./controller.js');
 //dbconnection 
-dbConnect();
+
 app.use(cors());
 var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -18,6 +19,21 @@ var allowCrossDomain = function (req, res, next) {
   app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 app.use(bodyParser.json({ limit: '50mb' }));
+
+
+let dev_db_url = 'mongodb+srv://sathish:hfvqi0ZP8c7cpqWQ@cluster0.urej5qj.mongodb.net/book';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+var options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(mongoDB, options);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function callback() {
+  console.log("MongoDB connection success");
+});
 // routes
 app.use('/',book)
 
