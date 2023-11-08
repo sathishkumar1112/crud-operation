12148663a57e7e4bb763b82104328051b321d225
Sathishkumar <sathishkumar@Sathishkumars-MacBook-Air.local>
2023-11-08 15:12:53 +0530

    connect changes

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -3,11 +3,33 @@ let dev_db_url = 'mongodb+srv://sathish:hfvqi0ZP8c7cpqWQ@cluster0.urej5qj.mongod
 
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 const connectDB = async () => {
-    try{
-    await mongoose.connect(mongoDB);
-    console.warn("MongoDB connection success");
-} catch(error){
-    console.error('MongoDB connection error:', error);
+    try {
+        const options = {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+    
+        await mongoose.connect(mongoDB, options);
+    
+        const db = mongoose.connection;
+    
+        db.on('error', (err) => {
+          console.error('MongoDB connection error:', err);
+        });
+    
+        db.once('open', () => {
+          console.log('MongoDB connection success');
+        });
+        
+    
+        return db; 
+    }
+        catch (error) {
+            console.error('MongoDB connection error:', error);
+            throw error; 
+          }
 }
-}
+
 module.exports = connectDB;
+// https://wild-red-coral-shoe.cyclic.app
+// https://wild-red-coral-shoe.cyclic.app
