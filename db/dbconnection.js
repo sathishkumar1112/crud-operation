0b1c65c097f3b0e208046eac98b3a76c97c7f3e7
Sathishkumar <sathishkumar@Sathishkumars-MacBook-Air.local>
2023-11-08 14:57:58 +0530

    added mongodb

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-let dev_db_url = 'mongodb://localhost:27017/test';
+let dev_db_url = 'mongodb+srv://sathish:hfvqi0ZP8c7cpqWQ@cluster0.urej5qj.mongodb.net/book';
+
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 const connectDB = async () => {
     try{
